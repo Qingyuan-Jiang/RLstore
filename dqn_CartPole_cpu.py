25100a5e4305a0e479ac24f2fa38a8abcc969702
Qingyuan Jiang <jian0345@umn.edu>
2020-06-02 12:59:46 -0500

    results

--- dqn_CartPole_cpu.py
+++ dqn_CartPole_cpu.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
 
     # Training parameters.
     dqn_algo(env_name='CartPole-v1', device=device,
-             steps_per_epoch=1000, epochs=50, max_ep_len=1000, replay_size=int(1e4), batch_size=256,
+             steps_per_epoch=1000, epochs=200, max_ep_len=1000, replay_size=int(1e4), batch_size=256,
              start_steps=1000, update_after=1000, update_every=20, target_freq=5,
              gamma=0.999, epsilon_start=0.8, epsilon_final=0.05, save_freq=5)
