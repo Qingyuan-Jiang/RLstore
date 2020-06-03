474cd12739553ec67195700b980949bf5ec633c2
Qingyuan Jiang <jian0345@umn.edu>
2020-06-03 15:09:53 -0500

    mountain car

--- dqn_MountainCar_cpu.py
+++ dqn_MountainCar_cpu.py
@@ -17,10 +17,15 @@ if __name__ == '__main__':
     device = "cpu"
 
     # Params
-    replay_size = int(1e4)
-    batch_size = 256
-    start_steps = 100
-    update_after = 100
+    replay_size = int(5e4)
+
+    steps_per_epoch = 1000
+    epochs = 200
+    max_ep_len = 999
+
+    batch_size = 512
+    start_steps = 1000
+    update_after = 1000
     update_every = 20
     target_freq = 5
     save_freq = 5
@@ -29,9 +34,6 @@ if __name__ == '__main__':
     epsilon_start = 0.8
     epsilon_final = 0.005
 
-    steps_per_epoch = 100
-    epochs = 20
-    max_ep_len = 1000
     save_path = 'model/'
 
     env = gym.make('MountainCar-v0')
@@ -61,16 +63,9 @@ if __name__ == '__main__':
         ag.Q_targ.eval()
     '''
 
-    # EP_ret, traj_ret = [], []
     test_ret, test_len = [], []
     test_win_len = 10
     test_ret_win = []
-
-    # fig, ax = plt.subplots()
-    # ax.set_title('Agent Returns of the epoch.')
-    # ax.set_xlabel('Epoch')
-    # ax.set_ylabel('Avg Returns')
-    # ax.grid(True)
 
     for t in range(total_steps):
 
@@ -86,28 +81,18 @@ if __name__ == '__main__':
 
         # Execute a in the environment.
         obs_next, r, d, info = env.step(a)
-        # x_next = encoder(obs_next)
-
-        ep_ret = ep_ret + r
-        # ep_len = ep_len + 1
         env.render()
 
-        # d = False if ep_len == max_ep_len else d
+        d = False if ep_len == max_ep_len else d
 
         # Store experience to replay buffer
         replay_buffer.store(obs, a, r, obs_next, d)
-        # replay_buffer.store(x, a, r, x_next, d)
 
         obs = obs_next
-        # x = x_next
 
         # End of trajectory
         if d or (ep_len == max_ep_len):
-            # obs, ep_ret, ep_len = env.reset(), 0, 0
             obs = env.reset()
-            # x = encoder(obs)
-            # traj_ret.append(ep_ret)
-            # ep_ret = 0
 
         # Update of the agent
         if t >= update_after and t % update_every == 0:
@@ -123,20 +108,11 @@ if __name__ == '__main__':
             if (epoch % save_freq == 0) or (epoch == epochs):
                 torch.save(ag.Q.state_dict(), PATH)
 
-            # if epoch % target_freq == 0:
-            #     ag.Q_targ.load_state_dict(ag.Q.state_dict())
-            #     # ag.Q_targ.eval()
-
-            # EP_ret.append(np.average(traj_ret))
-            # ax.plot(EP_ret, 'b')
-            # plt.pause(0.001)
-
-            # traj_ret = []
+            if epoch % target_freq == 0:
+                ag.Q_targ.load_state_dict(ag.Q.state_dict())
+                # ag.Q_targ.eval()
 
             epsilon = epsilon_final + math.exp(-1 * epoch / epochs) * (epsilon_start - epsilon_final)
-
-            # self.test_epoch = self.test_epoch + 1
-            # print("##### Epoch", self.test_epoch, "test rets: ", avg_ret, "with epi. length: ", avg_len, "#####")
 
             avg_ret, avg_len = ag.test_agent(env_test)
             test_ret.append(avg_ret)
@@ -147,8 +123,7 @@ if __name__ == '__main__':
             else:
                 ret_win = test_ret[-test_win_len:]
                 test_ret_win.append(np.average(ret_win))
-            x = np.arange(len(test_ret))
-            plt.scatter(x, avg_ret, c='green', marker='o', label='Test returns')
+            plt.scatter(len(test_ret)-1, avg_ret, c='green', marker='o', label='Test returns')
             plt.plot(test_ret_win, '-b', label='Average returns', linewidth=2)
             plt.pause(0.001)
             plt.axis(True)
